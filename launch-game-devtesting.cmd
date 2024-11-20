b70d245c88f844b03387c56156ee75cecab86bed
PhoebeIV <126877688+PhoebeIV@users.noreply.github.com>
2024-11-20 23:25:53 +1100

    remove pointless subfolder

---
+++ launch-game-devtesting.cmd
@@ -0,0 +1,24 @@
+@echo off
+title OpenRA
+for /F "delims==\ " %%x in ("%*") do (
+  if "%%~x" EQU "Game.Mod" (goto launch)
+)
+
+cd %~dp0%
+bin\OpenRA.exe Engine.EngineDir=".." Engine.LaunchPath="%~dpf0" Game.Mod=ow Debug.DisplayDeveloperSettings=true Launch.Map=testingmap
+
+:end
+if %errorlevel% neq 0 goto crashdialog
+exit /b
+
+:crashdialog
+set logs=%AppData%\OpenRA\Logs
+if exist %USERPROFILE%\Documents\OpenRA\Logs (set logs=%USERPROFILE%\Documents\OpenRA\Logs)
+if exist Support\Logs (set logs=%cd%\Support\Logs)
+
+echo ----------------------------------------
+echo OpenRA has encountered a fatal error.
+echo   * Log Files are available in %logs%
+echo   * FAQ is available at https://github.com/OpenRA/OpenRA/wiki/FAQ
+echo ----------------------------------------
+pause
